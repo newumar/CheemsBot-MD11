3a99041f028e57aef7f175287b7def797bf887ac
newumar <163923890+newumar@users.noreply.github.com>
2024-03-19 06:50:14 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,12 +16,12 @@ global.sessionid ='ur session id'
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Africa, logos, jigawa" //ur location
 
 //new
 global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.ownernumber = '2348151657681' //ur owner number
+global.ownername = 'Bot deployer' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
